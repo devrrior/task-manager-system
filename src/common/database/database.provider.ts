0670651a9173a504e1b69ec3bc615eed3627cd18
Fernando Guerrero <devrrior@gmail.com>
2024-07-17 07:23:24 -0600

    feat(database.provider): Add detecte auto entities

--- src/common/database/database.provider.ts
+++ src/common/database/database.provider.ts
@@ -1,8 +1,3 @@
-import { Comment } from 'src/modules/comments/entities/comment.entity';
-import { Log } from 'src/modules/logs/entities/log.entity';
-import { Tag } from 'src/modules/tags/entities/tag.entity';
-import { Task } from 'src/modules/tasks/entities/task.entity';
-import { User } from 'src/modules/users/entities/user.entity';
 import { DataSource } from 'typeorm';
 
 export const databaseProviders = [
@@ -16,8 +11,9 @@ export const databaseProviders = [
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        entities: [User, Task, Comment, Tag, Log],
-        synchronize: false,
+        entities: [__dirname + '/../../**/*.entity.{js,ts}'],
+        synchronize: true,
+        migrationsRun: false,
       });
 
       return dataSource.initialize();
